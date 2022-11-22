587e0433fd46f03c53afc1de6f93eaf9f30942ea
lamantur <Lamantur2017@gmail.com>
2022-11-22 13:47:26 +0300

    3

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 3: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Введите число");
+string input_Value = Console.ReadLine();
+int Value = Convert.ToInt32(input_Value);
+
+if (Value % 2 == 0)
+{
+    System.Console.WriteLine("ДА");
+}
+else
+{
+    System.Console.WriteLine("НЕТ");
+}
